c2906953c950946d45d48ec09fce6fda67f90559
claudiu_mihai_toma <13257303+claudiu-mihai-toma@users.noreply.github.com>
2015-08-14 00:43:56 +0300

    Very good version of Android client (Framework).

--- ProtocolTesterFramework/app/src/main/java/claudiu/protocoltesterframework/AbstractTest.java
+++ ProtocolTesterFramework/app/src/main/java/claudiu/protocoltesterframework/AbstractTest.java
@@ -1,5 +1,7 @@
 package claudiu.protocoltesterframework;
 
+import android.util.Log;
+
 import java.io.IOException;
 import java.net.Socket;
 
@@ -8,9 +10,13 @@ import java.net.Socket;
  */
 public abstract class AbstractTest {
 
-    private int mRepeats = 10;
+    protected int mRepeats = 10;
+    protected int[] sleepingTime = {1, 5, 10, 15, 20, 25, 30, 40, 50, 60, 70, 80, 90, 100, 200, 300, 400, 500};
 
     protected TcpSocket mTCPSocket;
+
+    protected void changeParams() {
+    }
 
     /**
      * @throws IOException for easy use of send and receive
@@ -18,16 +24,25 @@ public abstract class AbstractTest {
     public abstract void test() throws IOException;
 
     public void runTests(Socket socket) throws IOException {
+        changeParams();
         connect(socket);
-
-        for (int i = 0; i < mRepeats; i++) {
-            test();
+        for (int index = 0; index < sleepingTime.length; index++) {
+            Log.d(MainActivity.FRAMEWORK, "Delay: " + sleepingTime[index] + " ms");
+            for (int repeat = 0; repeat < mRepeats; repeat++) {
+                test();
+                Log.d(MainActivity.FRAMEWORK, "Test number: " + (repeat + 1));
+//                try {
+//                    Thread.sleep(sleepingTime[index]);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+            }
         }
-
         disconnect();
+        Log.d(MainActivity.FRAMEWORK, "Tests [" + this.toString() + "] ended.");
     }
 
-    public abstract void GetResults();
+    public abstract void getResults();
 
     public void connect(Socket socket) throws IOException {
         mTCPSocket = new TcpSocket(socket);
