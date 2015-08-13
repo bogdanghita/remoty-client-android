729c6434dafbac559428c2d242ca5da90f047958
Bogdan Ghita <bogdan.ghita93@gmail.com>
2015-08-13 12:19:56 +0300

    Handled exceptions and added documentation

--- ProtocolTester/app/src/main/java/com/example/bogdan/apis/TcpSocket.java
+++ ProtocolTester/app/src/main/java/com/example/bogdan/apis/TcpSocket.java
@@ -11,6 +11,7 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.net.SocketException;
 
 /**
  * Created by Bogdan on 8/6/2015.
@@ -18,31 +19,84 @@ import java.net.Socket;
 public class TcpSocket {
 
 	public final static String JSON = "JSON";
+	private final static String IMPOSSIBLE = "Impossible just happened.";
 
-	Socket mSocket;
-	DataInputStream mReader;
-	DataOutputStream mWriter;
+	private Socket mSocket;
+	private DataInputStream mReader;
+	private DataOutputStream mWriter;
 
-	Gson mGson;
+	private Gson mGson;
 
-	// TODO: add functionality for setting the timeout
-	/*
-		NOTE: mSocket must be already connected!
+	/**
+	 * @param socket  socket connected to a remote host address and port
+	 * @param timeout read timeout in milliseconds (0 means no timeout)
+	 * @throws IOException
 	 */
-	public TcpSocket(Socket socket) throws IOException {
+	public TcpSocket(Socket socket, int timeout) throws IOException {
 
 		this.mSocket = socket;
 
-		mGson = new Gson();
-
-		socket.setKeepAlive(true);
-		socket.setTcpNoDelay(true);
+		socket.setSoTimeout(timeout);
 
 		mReader = new DataInputStream(socket.getInputStream());
 		mWriter = new DataOutputStream(socket.getOutputStream());
+
+		mGson = new Gson();
 	}
 
-	// TODO: think of the flush call
+	/**
+	 * Sets this socket's read timeout
+	 *
+	 * @param timeout read timeout in milliseconds (0 means no timeout)
+	 * @throws SocketException
+	 */
+	public void setTimeout(int timeout) throws SocketException {
+		mSocket.setSoTimeout(timeout);
+	}
+
+	/**
+	 * Sets this socket's SO_KEEPALIVE option.
+	 *
+	 * @param keepAlive
+	 * @throws SocketException
+	 */
+	public void setKeepAlive(boolean keepAlive) throws SocketException {
+
+		mSocket.setKeepAlive(keepAlive);
+	}
+
+	/**
+	 * Sets this socket's TCP_NODELAY option
+	 *
+	 * @param on
+	 * @throws SocketException
+	 */
+	public void setTcpNoDelay(boolean on) throws SocketException {
+
+		mSocket.setTcpNoDelay(true);
+	}
+
+	/**
+	 * Closes the socket
+	 *
+	 * @throws IOException
+	 */
+	public void close() throws IOException {
+
+		if (!mSocket.isClosed()) {
+			mSocket.close();
+		}
+
+		mReader.close();
+		mWriter.close();
+	}
+
+	/**
+	 * Sends a message in the form of byte array
+	 *
+	 * @param content the content to be sent
+	 * @throws IOException
+	 */
 	public void send(byte[] content) throws IOException {
 
 		mWriter.writeInt(content.length);
@@ -52,6 +106,12 @@ public class TcpSocket {
 		mWriter.flush();
 	}
 
+	/**
+	 * Receives a message in the form of byte array
+	 *
+	 * @return the content received
+	 * @throws IOException
+	 */
 	public byte[] receive() throws IOException {
 
 		int size = mReader.readInt();
@@ -68,16 +128,15 @@ public class TcpSocket {
 		int cnt = 0;
 		int currentSize = 0;
 
-		// TODO: run a test and check when mReader returns -1
 		while (currentSize != size) {
 
 			cnt++;
 
 			int bytes_read = mReader.read(buffer, currentSize, size - currentSize);
 
-			// TODO: think what to do in this case
+			/* This should not happen. Even Android Studio marks any breakpoint inside this if statement as invalid. */
 			if (bytes_read == -1) {
-				continue;
+				throw new IOException(IMPOSSIBLE);
 			}
 
 			currentSize += bytes_read;
@@ -86,6 +145,12 @@ public class TcpSocket {
 		Log.d("TIME", "Fragments: " + cnt);
 	}
 
+	/**
+	 * Sends an object of type AbstractMessage or any subclass of it
+	 *
+	 * @param message the object to be sent
+	 * @throws IOException
+	 */
 	public void sendObject(AbstractMessage message) throws IOException {
 
 		long start = System.currentTimeMillis();
@@ -100,6 +165,15 @@ public class TcpSocket {
 		send(content);
 	}
 
+	/**
+	 * Receives a message of type AbstractMessage or any subclass of it
+	 *
+	 * @param type the type of the message that is received
+	 * @param <T>  the type of the message that is received
+	 * @return the object received
+	 * @throws IOException
+	 * @throws ClassNotFoundException
+	 */
 	public <T extends AbstractMessage> T receiveObject(Class<T> type) throws IOException, ClassNotFoundException {
 
 		byte[] content = receive();
@@ -116,16 +190,15 @@ public class TcpSocket {
 		return message;
 	}
 
-	// Receives image directly from the network (it seems Android can receive only images in .png format using this method)
+	/**
+	 * Receives image directly from the network (it seems Android can receive only images in .png format using this method)
+	 *
+	 * @return the image received
+	 */
 	public Bitmap receiveBitmap() {
 
 		Bitmap bitmap = BitmapFactory.decodeStream(mReader);
 
 		return bitmap;
-	}
-
-	public void close() throws IOException {
-
-		mSocket.close();
 	}
 }
