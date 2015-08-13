df276dfe855ccf5c4a4fa70d4b3b1d22c9b99c42
claudiu_mihai_toma <claudiu_mihai_toma@localhost>
2015-08-14 00:47:01 +0300

    Forgot a file. See previous commit for details.

---
+++ ProtocolTesterFramework/app/src/main/java/claudiu/protocoltesterframework/BigDataTest.java
@@ -0,0 +1,25 @@
+package claudiu.protocoltesterframework;
+
+import android.util.Log;
+
+import java.io.IOException;
+
+/**
+ * Created by Claudiu on 13/Aug/2015.
+ */
+public class BigDataTest extends AbstractTest {
+    @Override
+    public void test() throws IOException {
+        byte[] message = mTCPSocket.receive();
+        mTCPSocket.send(message);
+    }
+
+    @Override
+    public void getResults() {
+    }
+
+    @Override
+    protected void changeParams() {
+        mRepeats = 30;
+    }
+}
