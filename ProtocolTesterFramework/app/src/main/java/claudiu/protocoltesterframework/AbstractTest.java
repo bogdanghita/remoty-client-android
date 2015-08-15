8269d4fd198b1eb7bb07ad07d46db358675c93de
claudiu_mihai_toma <claudiu_mihai_toma@localhost>
2015-08-16 01:16:28 +0300

    Improvements and tests

--- ProtocolTesterFramework/app/src/main/java/claudiu/protocoltesterframework/AbstractTest.java
+++ ProtocolTesterFramework/app/src/main/java/claudiu/protocoltesterframework/AbstractTest.java
@@ -4,6 +4,7 @@ import android.util.Log;
 
 import java.io.IOException;
 import java.net.Socket;
+import java.net.SocketException;
 
 /**
  * Created by Claudiu on 13/Aug/2015.
@@ -12,10 +13,11 @@ public abstract class AbstractTest {
 
     protected int mRepeats = 10;
     protected int[] sleepingTime = {1, 5, 10, 15, 20, 25, 30, 40, 50, 60, 70, 80, 90, 100, 200, 300, 400, 500};
+    protected final static int NO_TIMEOUT = 0;
 
     protected TcpSocket mTCPSocket;
 
-    protected void changeParams() {
+    protected void changeParams() throws SocketException {
     }
 
     /**
@@ -24,8 +26,8 @@ public abstract class AbstractTest {
     public abstract void test() throws IOException;
 
     public void runTests(Socket socket) throws IOException {
-        changeParams();
         connect(socket);
+        changeParams();
         for (int index = 0; index < sleepingTime.length; index++) {
             Log.d(MainActivity.FRAMEWORK, "Delay: " + sleepingTime[index] + " ms");
             for (int repeat = 0; repeat < mRepeats; repeat++) {
@@ -45,7 +47,9 @@ public abstract class AbstractTest {
     public abstract void getResults();
 
     public void connect(Socket socket) throws IOException {
-        mTCPSocket = new TcpSocket(socket);
+        Log.d(MainActivity.FRAMEWORK, "Creating TCP socket...");
+        mTCPSocket = new TcpSocket(socket,NO_TIMEOUT);
+        Log.d(MainActivity.FRAMEWORK, "TCP socket created!");
     }
 
     public void disconnect() throws IOException {
--- ProtocolTesterFramework/app/src/main/java/claudiu/protocoltesterframework/ParallelTest.java
+++ ProtocolTesterFramework/app/src/main/java/claudiu/protocoltesterframework/ParallelTest.java
@@ -8,12 +8,14 @@ import java.net.SocketException;
 /**
  * Created by Claudiu on 13/Aug/2015.
  */
-public class BigDataTest extends AbstractTest {
+public class ParallelTest extends AbstractTest {
+    private final int parallels = 5;
+
     @Override
     protected void changeParams() throws SocketException {
-        mRepeats = 5;
-        mTCPSocket.setKeepAlive(false);
-        //mTCPSocket.setKeepAlive(true);
+        mRepeats = 10;
+        //mTCPSocket.setKeepAlive(false);
+        mTCPSocket.setKeepAlive(true);
         //mTCPSocket.setTcpNoDelay(false);
         mTCPSocket.setTcpNoDelay(true);
         mTCPSocket.setTimeout(10000);
@@ -21,9 +23,14 @@ public class BigDataTest extends AbstractTest {
 
     @Override
     public void test() throws IOException {
-        byte[] message = mTCPSocket.receive();
+        byte[] message = new byte[0];
+
+        for (int i = 0; i < parallels; i++)
+            message = mTCPSocket.receive();
         Log.d(MainActivity.FRAMEWORK, "Message sent.");
-        mTCPSocket.send(message);
+
+        for (int i = 0; i < parallels; i++)
+            mTCPSocket.send(message);
         Log.d(MainActivity.FRAMEWORK, "Message received.");
     }
 
