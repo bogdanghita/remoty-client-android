60214a77cc106ae0ff55fb1f579a75d50eb2cca0
Bogdan Ghita <17732325+bogdanghita@users.noreply.github.com>
2015-08-17 01:32:47 +0300

    Added Remoty app. Working on the navigation/flow

---
+++ Remoty/app/src/main/java/com/remoty/DriveFragment.java
@@ -0,0 +1,23 @@
+package com.remoty;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Bogdan on 8/17/2015.
+ */
+public class DriveFragment extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+		setHasOptionsMenu(true);
+
+		View parentView = inflater.inflate(R.layout.fragment_drive, container, false);
+
+		return parentView;
+	}
+}
