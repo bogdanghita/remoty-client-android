bdf722dda421a3f2a9f1b4f2296c439c00ab3464
Bogdan Ghita <bogdan.ghita93@gmail.com>
2015-08-22 02:06:52 +0300

    Added debug clases and app description

--- Remoty/app/src/main/java/com/remoty/MainFragment.java
+++ Remoty/app/src/main/java/com/remoty/MainFragment.java
@@ -1,7 +1,6 @@
 package com.remoty;
 
 import android.os.Bundle;
-import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,16 +9,16 @@ import android.view.ViewGroup;
 /**
  * Created by Bogdan on 8/16/2015.
  */
-public class MainFragment extends Fragment {
+public class MainFragment extends DebugFragment {
 
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-		setHasOptionsMenu(true);
+        setHasOptionsMenu(true);
 
-		View parentView = inflater.inflate(R.layout.fragment_main, container, false);
+        View parentView = inflater.inflate(R.layout.fragment_main, container, false);
 
-		return parentView;
-	}
+        return parentView;
+    }
 
 }
