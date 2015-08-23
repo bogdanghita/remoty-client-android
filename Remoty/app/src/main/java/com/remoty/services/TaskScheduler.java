76d1136529c306612752c997d962a6c08252ecfa
Bogdan Ghita <bogdan.ghita93@gmail.com>
2015-08-23 17:06:43 +0300

    Updated and tested the TaskScheduler (former MyTimer)

---
+++ Remoty/app/src/main/java/com/remoty/services/TaskScheduler.java
@@ -0,0 +1,73 @@
+package com.remoty.services;
+
+import android.util.Log;
+
+/**
+ * Created by Bogdan on 8/22/2015.
+ */
+public class TaskScheduler {
+
+    public final static String TAG_TIMER = "TIMER";
+
+    private SchedulerThread mThread;
+
+    public void setInterval(long interval) {
+
+        if (mThread != null) {
+            mThread.setInterval(interval);
+        }
+    }
+
+    /**
+     * Starts the execution of the task, on a separate thread and at the given interval.
+     * The interval can be modified using the setInterval(long) method.
+     *
+     * @param task     - the task that will be executed at the given interval.
+     * @param interval - the interval at which the task is executed.
+     * @throws IllegalArgumentException    - if interval is less than or equal to zero.
+     * @throws IllegalThreadStateException - if the scheduler was already started.
+     */
+    public void start(Runnable task, long interval) {
+
+        if (mThread != null) {
+            throw new IllegalThreadStateException();
+        }
+
+        if (interval <= 0) {
+            throw new IllegalArgumentException();
+        }
+
+        mThread = new SchedulerThread(task, interval);
+
+        mThread.start();
+    }
+
+    /**
+     * TODO: See if you want this to join the thread or not. Maybe a parameter would help.
+     * TODO: Think if we want to also stop the task that is currently executed by calling mThread.interrupt()
+     * Stops the execution of the task and the thread that it was running on.
+     *
+     * @throws IllegalThreadStateException - if the scheduler is not started or was already stopped.
+     */
+    public void stop() {
+
+        if (mThread == null) {
+            throw new IllegalThreadStateException();
+        }
+
+        mThread.quit();
+
+        Log.d(TaskScheduler.TAG_TIMER, "mThread quit. Waiting for mThread to join.");
+
+        try {
+            mThread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            Log.d(TaskScheduler.TAG_TIMER, e.getClass().getName() + " | " + "exception on join()");
+        }
+
+        Log.d(TaskScheduler.TAG_TIMER, "mThread finished.");
+
+        mThread = null;
+    }
+}
