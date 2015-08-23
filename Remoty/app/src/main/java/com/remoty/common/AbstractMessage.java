1954de614d59d5dac50c29e4e516812f394cd412
claudiu_mihai_toma <claudiu_mihai_toma@localhost>
2015-08-23 23:43:34 +0300

    Early server detection alpha prototype

---
+++ Remoty/app/src/main/java/com/remoty/common/AbstractMessage.java
@@ -0,0 +1,15 @@
+package com.remoty.common;
+
+/**
+ * Created by Bogdan on 8/8/2015.
+ */
+public class AbstractMessage {
+
+    public long id;
+    public long timestamp;
+}
+
+class LargeMessage extends AbstractMessage {
+
+    public int[] data;
+}
