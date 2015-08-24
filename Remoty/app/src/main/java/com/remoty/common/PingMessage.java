2078002b9c3000c8f9ec8bba36e151004f36ff49
claudiu_mihai_toma <claudiu_mihai_toma@localhost>
2015-08-24 22:09:43 +0300

    ServerInfo is now populated (NOT tested).

---
+++ Remoty/app/src/main/java/com/remoty/common/PingMessage.java
@@ -0,0 +1,9 @@
+package com.remoty.common;
+
+/**
+ * Created by Claudiu on 24/Aug/2015.
+ */
+public class PingMessage extends AbstractMessage {
+
+    // TODO: This may be used for security purposes.
+}
