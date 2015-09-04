9e6eff3b5c99a8a4d0831e26cd0d196b89772500
Alina Mertoiu <alina.mertoiu@itsmartsystems.eu>
2015-09-04 23:59:30 +0300

    - reducing resources :D - added new fragments

---
+++ Remoty/app/src/main/java/com/remoty/gui/MarketFragment.java
@@ -0,0 +1,11 @@
+package com.remoty.gui;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by alina on 9/4/2015.
+ */
+public class MarketFragment extends Fragment {
+
+
+}
---
+++ Remoty/app/src/main/java/com/remoty/gui/SocialFragment.java
@@ -0,0 +1,9 @@
+package com.remoty.gui;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by alina on 9/4/2015.
+ */
+public class SocialFragment extends Fragment {
+}
