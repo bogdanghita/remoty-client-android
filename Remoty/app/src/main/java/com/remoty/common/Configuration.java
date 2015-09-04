b9482b8aafa4cdc8890faa21ba2eee07de6c4a51
Alina Mertoiu <alina.mertoiu@itsmartsystems.eu>
2015-09-04 23:59:30 +0300

    - reducing resources :D - added new fragments

---
+++ Remoty/app/src/main/java/com/remoty/common/Configuration.java
@@ -0,0 +1,21 @@
+package com.remoty.common;
+
+/**
+ * Created by alina on 9/4/2015.
+ */
+public class Configuration {
+
+    private String name;
+
+    public Configuration(){
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
