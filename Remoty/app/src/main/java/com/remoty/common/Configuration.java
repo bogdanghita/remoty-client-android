e3d6eb16de9be6e413f7ec8b54b626272c81d3e9
Bogdan Ghita <bogdan.ghita93@gmail.com>
2015-09-05 02:41:39 +0300

    [NewDesign][AppFlow] Organized the code a bit.

--- Remoty/app/src/main/java/com/remoty/common/Configuration.java
+++ Remoty/app/src/main/java/com/remoty/common/Configuration.java
@@ -1,21 +1,32 @@
 package com.remoty.common;
 
+import android.support.v4.app.Fragment;
+
 /**
  * Created by alina on 9/4/2015.
  */
 public class Configuration {
 
-    private String name;
+	private String name;
+	private Fragment fragment;
 
-    public Configuration(){
+	public Configuration() {
 
-    }
+	}
 
-    public String getName() {
-        return name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public String getName() {
+		return name;
+	}
+
+	public void setFragment(Fragment fragment) {
+		this.fragment = fragment;
+	}
+
+	public Fragment getFragment() {
+		return fragment;
+	}
 }
---
+++ Remoty/app/src/main/java/com/remoty/gui/MyConfigurationsFragment.java
@@ -0,0 +1,122 @@
+package com.remoty.gui;
+
+import android.support.v4.app.FragmentTransaction;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.remoty.R;
+import com.remoty.common.Configuration;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by alina on 9/4/2015.
+ */
+public class MyConfigurationsFragment extends DebugFragment {
+
+	LinearLayout configurations_layout;
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+		setHasOptionsMenu(true);
+
+		View parentView = inflater.inflate(R.layout.fragment_my_configurations, container, false);
+
+		configurations_layout = (LinearLayout) parentView.findViewById(R.id.configurations_layout);
+
+		// generateTestConfigurations() will be replaced by the actual user configurations
+		updateConfigurations(generateTestConfigurations());
+
+		return parentView;
+	}
+
+	private void updateConfigurations(List<Configuration> configurations) {
+
+		configurations_layout.removeAllViews();
+
+		if (configurations.isEmpty()) {
+			TextView textView = new TextView(getActivity());
+			textView.setText("You don't have any configurations yet :( ");
+			configurations_layout.addView(textView);
+			return;
+		}
+
+		for (Configuration config : configurations) {
+
+			Button button = createConfigurationButton(config);
+			configurations_layout.addView(button);
+		}
+	}
+
+	private Button createConfigurationButton(final Configuration config) {
+
+		Button button = new Button(MainActivity.Instance);
+
+		LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+		button.setLayoutParams(params);
+
+		button.setTextColor(Color.DKGRAY);
+
+		button.setPadding(0, 0, 0, 10);
+
+		button.setText(config.getName());
+
+		button.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+
+				Fragment fragment = config.getFragment();
+
+				switchToFragment(fragment);
+			}
+		});
+
+		return button;
+	}
+
+	private void switchToFragment(Fragment fragment) {
+
+		// TODO: Open the new fragment.
+		// NOTE: A configuration page is going to be launched so the screen should not contain the action bar and the tabs
+		// You should either hide them and show them back when we return to the previous fragment (this can be problematic...)
+		// or we should define a different layout for the configurations fragments which will replace the layout of the main
+		// activity (I think this is better because the layouts will be automatically restored when the fragment closes)
+		// This needs research...
+
+		FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+
+		// TODO: This is strange... the Drive button remains there...
+		transaction.replace(R.id.fragment_my_configurations, fragment);
+
+		transaction.addToBackStack(null);
+		transaction.commit();
+	}
+
+	//TEST METHODS
+
+	public List<Configuration> generateTestConfigurations() {
+
+		List<Configuration> configurations = new LinkedList<>();
+
+		Configuration c = new Configuration();
+		c.setName("Drive");
+		c.setFragment(new DriveFragment());
+
+		configurations.add(c);
+
+		return configurations;
+	}
+
+	//END TEST METHODS
+}
