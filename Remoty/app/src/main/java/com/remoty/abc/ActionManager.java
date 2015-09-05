5d69de73e7d205c59277c103ed0d82f2c953a9e5
Bogdan Ghita <bogdan.ghita93@gmail.com>
2015-09-06 00:54:14 +0300

    [ApplicationStructure][Refactor] - created new application structure - detection working - not completed

---
+++ Remoty/app/src/main/java/com/remoty/abc/ActionManager.java
@@ -0,0 +1,49 @@
+package com.remoty.abc;
+
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+
+import com.remoty.common.AccelerometerService;
+import com.remoty.common.RemoteControlService;
+import com.remoty.services.detection.DetectionService;
+
+/**
+ * Created by Bogdan on 9/5/2015.
+ */
+public class ActionManager {
+
+	// TODO: This class is similar to a factory class. Think about this...
+
+	private DetectionService serverDetection;
+
+	private ConnectionCheckService connectionCheck;
+
+	private RemoteControlService remoteControl;
+
+	public ActionManager(EventManager eventManager) {
+
+		serverDetection = new DetectionService(eventManager);
+		connectionCheck = new ConnectionCheckService();
+		remoteControl = new RemoteControlService();
+	}
+
+	public DetectionService getServerDetectionService() {
+
+		return serverDetection;
+	}
+
+	public ConnectionCheckService getConnectionCheckService() {
+
+		return connectionCheck;
+	}
+
+	public RemoteControlService getRemoteControlService() {
+
+		return remoteControl;
+	}
+
+	public AccelerometerService getAccelerometerService(SensorManager sensorManager, Sensor accelerometerSensor) {
+
+		return new AccelerometerService(sensorManager, accelerometerSensor);
+	}
+}
---
+++ Remoty/app/src/main/java/com/remoty/abc/EventManager.java
@@ -0,0 +1,50 @@
+package com.remoty.abc;
+
+import com.google.common.collect.HashMultimap;
+import com.remoty.abc.events.ConnectionCheckListener;
+import com.remoty.abc.events.DetectionListener;
+import com.remoty.abc.events.IServiceEventListener;
+import com.remoty.abc.events.ServiceEvent;
+
+/**
+ * Created by Bogdan on 9/5/2015.
+ */
+public class EventManager {
+
+	HashMultimap<EventType, IServiceEventListener> listeners;
+
+	public EventManager() {
+
+		listeners = HashMultimap.create();
+	}
+
+	public enum EventType {
+		CONNECTION_CHECK,
+		DETECTION
+	}
+
+	public void subscribe(ConnectionCheckListener l) {
+
+		listeners.put(EventType.CONNECTION_CHECK, l);
+	}
+
+	public void unsubscribe(ConnectionCheckListener l) {
+		listeners.put(EventType.CONNECTION_CHECK, l);
+	}
+
+	public void subscribe(DetectionListener l) {
+		listeners.put(EventType.DETECTION, l);
+	}
+
+	public void unsubscribe(DetectionListener l) {
+		listeners.put(EventType.DETECTION, l);
+	}
+
+	public void triggerEvent(ServiceEvent event) {
+
+		for (IServiceEventListener l : listeners.get(event.getType())) {
+			l.notify(event);
+		}
+	}
+
+}
