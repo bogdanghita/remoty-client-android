330601fbeb07adef7c25c7aad863c5795ca408b7
Bogdan Ghita <bogdan.ghita93@gmail.com>
2015-09-05 10:39:27 +0300

    [NewDesign][AppFlow] Organized the code a bit more.

--- Remoty/app/src/main/java/com/remoty/gui/SocialFragment.java
+++ Remoty/app/src/main/java/com/remoty/gui/SocialFragment.java
@@ -5,5 +5,5 @@ import android.support.v4.app.Fragment;
 /**
  * Created by alina on 9/4/2015.
  */
-public class SocialFragment extends Fragment {
+public class SocialFragment extends DebugFragment {
 }
